6f38910cd60569ee975e22f41d30e31b81c6675c
AshishThakur10 <sendashishthakur@gmail.com>
2021-04-03 22:13:44 +0530

    create database update
    added example

--- Query/CREATEDATABASE.sql
+++ Query/CREATEDATABASE.sql
@@ -6,3 +6,15 @@ CREATE DATABASE name
            [ LC_CTYPE [=] lc_ctype ]
            [ TABLESPACE [=] tablespace ]
            [ CONNECTION LIMIT [=] connlimit ] ]
+
+
+example
+
+CREATE DATABASE "Test"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "Test"
+    IS 'this server is created for learning perpouse';
